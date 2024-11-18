1097eefc628660bc5aa875bf2cdfb3359585c62f
syedzohaibalikazmi <syedzohaibali9900@gmail.com>
2024-11-18 21:29:17 +0500

    Learning Next-JS

--- 1-next/src/app/page.js
+++ 1-next/src/app/page.js
@@ -1,10 +1,12 @@
+import HeroSection from "@/components/HeroSection";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div className="text-white">
       <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
-      <h1 className="text-center">Hello Next JS </h1>
+ 
+      <HeroSection/>
       </main>
     </div>
   );
--- 1-next/tailwind.config.js
+++ 1-next/tailwind.config.js
@@ -8,9 +8,24 @@ module.exports = {
   darkMode: 'class',
   theme: {
     extend: {
+      animation: {
+        spotlight: "spotlight 2s ease .75s 1 forwards",
+      },
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+      },
+      keyframes: {
+        spotlight: {
+          "0%": {
+            opacity: 0,
+            transform: "translate(-72%, -62%) scale(0.5)",
+          },
+          "100%": {
+            opacity: 1,
+            transform: "translate(-50%,-40%) scale(2)",
+          },
+        },
       },
     },
   },
